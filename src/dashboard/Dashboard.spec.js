f0cdff0dc3ecaede4ae7c236b4abf0dad136fd16
Aaron Spurgeon <aspurgeon061@gmail.com>
2019-10-30 20:15:50 -0400

    Created initial tests, still dont really know what Im doing, need to read more up on react testing

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -1,1 +1,18 @@
 // Test away
+
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import { getByTestId } from "@testing-library/dom";
+import Dashboard from "./Dashboard";
+
+afterEach(rtl.cleanup);
+
+test("Display and Controls component rendering", () => {
+  const wrapper = rtl.render(<Dashboard />);
+  const display = getByTestId("display");
+  const controls = getByTestId("controls");
+
+  expect(display).toBeCalled();
+  expect(controls).toBeCalled();
+});
--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,13 @@
 // Test away!
+import React from "react";
+import * as rtl from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+import Display from "./Display";
+
+afterEach(rtl.cleanup);
+
+// test('<Display /> snapshot', async () => {
+//     const wrapper = rtl.render(<Display />)
+//     await wrapper.findAllBy
+// })
+test();
